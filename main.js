f7ee654ccc7c3b7a8874ce5de268e80346a55e94
willem <1121567132@qq.com>
2022-09-17 18:44:51 +0800

    改进convolution；实现高斯滤波；实现NMS

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-import { getImageData, pixelTraversal, matrixTraversal, grayScale, drawImageFromArray, convolution, sobel, arrayDivide, expandToImageDataArray, normalization } from './src/util.js'
+import { nms, getImageData, pixelTraversal, matrixTraversal, grayScale, drawImageFromArray, convolution, sobel, arrayDivide, expandToImageDataArray, normalization, gaussianFilter } from './src/util.js'
 
 const canvas = document.getElementById('myCanvas') // canvas画布
 const canvasGray = document.getElementById('myCanvasGray') // canvas画布
 const canvasEdgeX = document.getElementById('myCanvasEdgeX') // canvas画布
 const canvasEdgeY = document.getElementById('myCanvasEdgeY') // canvas画布
 const canvasEdgeTotal = document.getElementById('myCanvasEdgeTotal') // canvas画布
+const canvasGaussian = document.getElementById('myCanvasGaussian') // canvas画布
+const canvasEdgeXS = document.getElementById('myCanvasEdgeXS') // canvas画布
+const canvasEdgeYS = document.getElementById('myCanvasEdgeYS') // canvas画布
+const canvasEdgeTotalS = document.getElementById('myCanvasEdgeTotalS') // canvas画布
+const myCanvasNMS = document.getElementById('myCanvasNMS') // canvas画布
+const myCanvasLast = document.getElementById('myCanvasLast') // canvas画布
 
 getImageData(canvas, './img/test.jpg').then((data) => {
   console.log('ImageDataArray:', data) // 打印输出像素数据
@@ -18,6 +24,7 @@ getImageData(canvas, './img/test.jpg').then((data) => {
   /* convolution(imgGrayDataArray, 1, 2, kernel) */
   const { gradXArray, gradYArray, gradTotalArray, thetaArray } = sobel(imgGrayDataArray, canvasGray.width, canvasGray.height)
 
+  /* 归一化处理 */
   const normalGradXArray = normalization(gradXArray)
   const normalGradYArray = normalization(gradYArray)
   const normalGradTotalArray = normalization(gradTotalArray)
@@ -29,4 +36,41 @@ getImageData(canvas, './img/test.jpg').then((data) => {
   drawImageFromArray(canvasEdgeX, gradImageXArray)
   drawImageFromArray(canvasEdgeY, gradImageYArray)
   drawImageFromArray(canvasEdgeTotal, gradImageTotalArray)
+
+  console.warn('----------------sobel over----------------')
+
+  /* canny 2*/
+  const imgGrayGaussianArray = gaussianFilter(imgGrayDataArray, 5, 1)
+  const imgGrayGaussianDataArray = expandToImageDataArray(imgGrayGaussianArray)
+  drawImageFromArray(canvasGaussian, imgGrayGaussianDataArray)
+
+  /* canny 3 */
+  const { gradXArray: gradXArrayS, gradYArray: gradYArrayS, thetaArray: thetaArrayS, gradTotalArray: gradTotalArrayS } = sobel(imgGrayGaussianDataArray, 296, 296)
+
+  /* 归一化处理 */
+  const normalGradXArrayS = normalization(gradXArrayS)
+  const normalGradYArrayS = normalization(gradYArrayS)
+  const normalGradTotalArrayS = normalization(gradTotalArrayS)
+
+  const gradImageXArrayS = expandToImageDataArray(normalGradXArrayS.map((v) => v * 255))
+  const gradImageYArrayS = expandToImageDataArray(normalGradYArrayS.map((v) => v * 255))
+  const gradImageTotalArrayS = expandToImageDataArray(normalGradTotalArrayS.map((v) => v * 255))
+
+  drawImageFromArray(canvasEdgeXS, gradImageXArrayS)
+  drawImageFromArray(canvasEdgeYS, gradImageYArrayS)
+  drawImageFromArray(canvasEdgeTotalS, gradImageTotalArrayS)
+
+  /* canny 4:nms */
+  /* const nmsArray = nms(gradTotalArrayS, gradXArrayS, gradYArrayS) */
+  /* let arr = new Array(16).fill(0) */
+  /* let arr1 = arr.map((v, i) => i) */
+  /* let arr2 = arr1.map((v, i) => Math.random()) */
+  /* let arr3 = arr.map((v, i) => Math.random()) */
+  /* window.nms = nms */
+  /* window.arr1 = arr1 */
+  /* window.arr2 = arr2 */
+  /* window.arr3 = arr3 */
+  /* nms(arr1, arr2, arr3) */
+
+  const gradNMSArray = nms(gradTotalArray, gradXArray, gradYArray)
 })
--- src/util.js
+++ src/util.js
@@ -65,6 +65,7 @@ function normalization(array) {
   return array.map((v) => Math.round((v - min) / (max - min)))
 }
 
+/* TODO 为什么不直接算出width和height 因为图像不一定是方形的 目前大部分函数以方形计算 不灵活 */
 function sobel(imgGrayDataArray, imgWidth, imgHeight) {
   const kernelX = [-1, 0, -1, -2, 0, +2, -1, 0, +1]
   const kernelY = [1, 2, 1, 0, 0, 0, -1, -2, -1]
@@ -92,9 +93,16 @@ function sobel(imgGrayDataArray, imgWidth, imgHeight) {
   return { gradXArray, gradYArray, thetaArray, gradTotalArray }
 }
 
+/* 使用此函数卷积的时候需要从1开始最后一个也不能要 最外面一圈都要去掉 */
+/* 只能卷积三个核的 */
+/* const arr = new Array(5).fill(0) */
+/* const arr = new Array(kernelLength).fill(0).map((v, i) => i - Math.floor(kernelLength / 2)) */
 function convolution(imgGrayDataArray, pixelPosX, pixelPosY, kernel) {
-  const fieldX = [pixelPosX - 1, pixelPosX, pixelPosX + 1] /* 向下是x方向，向右是y方向 */
-  const fieldY = [pixelPosY - 1, pixelPosY, pixelPosY + 1]
+  const kernelLength = Math.sqrt(kernel.length)
+  /* 中心是零的数组 */
+  const tmpArr = new Array(kernelLength).fill(0).map((v, i) => i - Math.floor(kernelLength / 2))
+  const fieldX = tmpArr.map((v) => v + pixelPosX) /* 向下是x方向，向右是y方向 */
+  const fieldY = tmpArr.map((v) => v + pixelPosY)
   const width = Math.sqrt(imgGrayDataArray.length / 4)
   let i = 0
   let res = 0
@@ -145,15 +153,40 @@ function arrayDivide(arrayY, arrayX) {
   return arrayY.slice().map((v, i) => Math.round(v / arrayX[i]))
 }
 
-/* https://zhuanlan.zhihu.com/p/445415462 */
-function gaussianFilter() {
-  const sigma = 0.5
-  dim = Math.round(3 * 2 * sigma + 1)
-  if (dim % 2 === 0) {
-    dim += 1
+/* https://zhuanlan.zhihu.com/p/445415462 用的二维高斯公式不是离散的，好像有问题*/
+/* https://blog.csdn.net/qq_45717425/article/details/120640688 */
+function gaussianFilter(imgDataArray, k, sigma) {
+  const gauss_kernel = []
+  const len = Math.sqrt(Math.floor(imgDataArray.length / 4))
+  const imgArray = []
+  const edgeBias = Math.floor(k / 2)
+  const zeroCenteredArray = new Array(k).fill(0).map((v, i) => i - Math.floor(k / 2))
+
+  /* 高斯核构造 */
+  for (const i of zeroCenteredArray) {
+    for (const j of zeroCenteredArray) {
+      /* 二维高斯函数离散公式 */
+      /* TODO i和j从几开始？ */
+      gauss_kernel.push(Math.exp(-(Math.pow(i - k - 1, 2) + Math.pow(j - k - 1, 2)) / (2 * Math.pow(sigma, 2))) / (2 * Math.PI * Math.pow(sigma, 2)))
+    }
   }
-  n1 = 1 / (2 * Math.pi * sigma ** 2)
-  n2 = -1 / (2 * sigma ** 2)
+
+  /* 高斯核归一化 否则值会很小显示不出来 */
+  const sum = gauss_kernel.reduce((pre, cur) => pre + cur)
+  /* 修改元数组 */
+  gauss_kernel.forEach((v, i, a) => {
+    a[i] = v / sum
+  })
+
+  //根据卷积核的大小不同，边缘减少的大小也不同 k=5的时候，要从2开始
+  for (let i = edgeBias; i < len - edgeBias; i++) {
+    for (let j = edgeBias; j < len - edgeBias; j++) {
+      imgArray.push(convolution(imgDataArray, i, j, gauss_kernel))
+    }
+  }
+
+  console.log('ImageGaussianArray', imgArray)
+  return imgArray
 }
 
 function radianToAngle(radian) {
@@ -161,4 +194,78 @@ function radianToAngle(radian) {
   return Math.round(radian * (180 / Math.PI))
 }
 
-export { radianToAngle, gaussianFilter, getImageData, pixelTraversal, matrixTraversal, grayScale, drawImageFromArray, transposition, convolution, sobel, expandToImageDataArray, arrayDivide, normalization }
+/* https://blog.csdn.net/weixin_39994296/article/details/110595624 */
+function nms(gradTotalArray, gradXArray, gradYArray) {
+  const len = gradTotalArray.length
+  const w = Math.sqrt(len)
+  const gradNMSArray = []
+  /* 去掉外圈一层 */
+  for (let r = 1; r < w - 1; r++) {
+    for (let c = 1; c < w - 1; c++) {
+      let index = r * w + c
+      /* 向下是x方向，向右是y方向 */
+      /* const fieldX = tmpArr.map((v) => v + r)  */
+      /* const fieldY = tmpArr.map((v) => v + c) */
+
+      /* 通过两个循环拿坐标 取外围点的梯度值 */
+      /* 根据梯度方向正反取需要的点的梯度值加上权重得到插值点的权重 */
+      /* for (const row of fieldX) { */
+      /*   for (const col of fieldY) { */
+      const gradX = gradXArray[index]
+      const gradY = gradYArray[index]
+      const gradTotal = gradTotalArray[index]
+      /* 点确定 该点的权重就确定了 */
+      /* y/x是斜率，对应斜率角 x/y则对应三角形的另一个角 将像素点间的距离当作1 那么亚像素点与相邻两点的距离就可以算出来（与基准十字上点的距离为x/y，与四角上点的距离为1-x/y）*/
+      const weight = Math.abs(gradX) / Math.abs(gradY) /* TODO 注意 权重是x/y 梯度方向三角形的另一个角*/
+      let grad1, grad2, grad3, grad4
+
+      /* 根据梯度方向插值 */
+
+      /* 规定向下为x方向 水平向右为y方向 */
+      /* y大于x的情况 */
+      /* 1. 找到插值点的邻接点 */
+      if (Math.abs(gradY) > Math.abs(gradX)) {
+        /* 十字线上两点确定 */
+        grad2 = gradTotalArray[r * w + (c - 1)] /* 等同于gradTotalArray[r][c-1] */
+        grad4 = gradTotalArray[r * w + (c + 1)]
+        /*  gradx和grady同号 */
+        if (gradXArray[r * w + c] * gradYArray[r * w + c] > 0) {
+          grad1 = gradTotalArray[(r - 1) * w + (c - 1)]
+          grad3 = gradTotalArray[(r + 1) * w + (c + 1)]
+        } else {
+          /*  gradx和grady异号 */
+          grad1 = gradTotalArray[(r + 1) * w + (c - 1)]
+          grad3 = gradTotalArray[(r - 1) * w + (c + 1)]
+        }
+      } else {
+        grad2 = gradTotalArray[(r - 1) * w + c] /* 等同于gradTotalArray[r-1][c] */
+        grad4 = gradTotalArray[(r + 1) * w + c]
+        if (gradX * gradY > 0) {
+          grad1 = gradTotalArray[(r - 1) * w + (c - 1)]
+          grad3 = gradTotalArray[(r + 1) * w + (c + 1)]
+        } else {
+          grad1 = gradTotalArray[(r - 1) * w + (c + 1)]
+          grad3 = gradTotalArray[(r + 1) * w + (c - 1)]
+        }
+      }
+      /* 2. 邻接点权重通过加权得到插值点的权重 */
+      /* 1和2要在同侧 3和4要在同侧 */
+      const gradVirtual1 = weight * grad1 + (1 - weight) * grad2
+      const gradVirtual2 = weight * grad3 + (1 - weight) * grad4
+
+      /* 判断当前点梯度方向上的梯度与最近像素点的梯度比是否为极大值(局部最大值) */
+      if (gradTotal >= gradVirtual1 && gradTotal >= gradVirtual2) {
+        gradNMSArray.push(gradTotal)
+      } else {
+        /* 非极大值抑制 */
+        gradNMSArray.push(0)
+      }
+    }
+    /* } */
+    /* } */
+  }
+  console.log('gradNMSArray', gradNMSArray)
+  return gradNMSArray
+}
+
+export { nms, radianToAngle, gaussianFilter, getImageData, pixelTraversal, matrixTraversal, grayScale, drawImageFromArray, transposition, convolution, sobel, expandToImageDataArray, arrayDivide, normalization }
